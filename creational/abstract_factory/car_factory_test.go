72fd1acacff10449eb7434462d99e2642d2c8edb
Fanan Dala <fanan.dala@yahoo.com>
2020-11-12 19:17:56 +0100

    test(abstract_factory): add vehicle interface test

--- creational/abstract_factory/car_factory_test.go
+++ creational/abstract_factory/car_factory_test.go
@@ -10,7 +10,7 @@ func TestCarFactory(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
+	//test luxury car
 	carVehicle, err := carFactory.NewVehicle(LuxuryCarType)
 	if err != nil {
 		t.Fatal(err)
@@ -21,8 +21,41 @@ func TestCarFactory(t *testing.T) {
 		t.Fatal("Struct assertion failed")
 	}
 
-	//validate that it was a sport bike returend
+	//validate that it was a luxury car returend
 	if luxuryCar.NumDoors() != 4 {
 		t.Fatal("Wrong car type generated")
+	}
+
+	if luxuryCar.(Vehicle).NumWheels() != 4 {
+		t.Fatal("Wrong number of wheels generated for luxury car")
+	}
+
+	if luxuryCar.(Vehicle).NumSeats() != 5 {
+		t.Fatal("Wrong number of seats generated for luxury car")
+	}
+	
+
+	//test family car
+	carVehicle, err = carFactory.NewVehicle(FamilyCarType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	familyCar, ok := carVehicle.(Car)
+	if !ok {
+		t.Fatal("Struct assertion failed")
+	}
+
+	//validate that it was a family car returend
+	if familyCar.NumDoors() != 6 {
+		t.Fatal("Wrong car type generated")
+	}
+
+	if familyCar.(Vehicle).NumWheels() != 4 {
+		t.Fatal("Wrong number of wheels generated for luxury car")
+	}
+
+	if familyCar.(Vehicle).NumSeats() != 5 {
+		t.Fatal("Wrong number of seats generated for luxury car")
 	}
 }
--- creational/abstract_factory/motorbike_factory_test.go
+++ creational/abstract_factory/motorbike_factory_test.go
@@ -10,7 +10,7 @@ func TestMotorbikeFactory(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
+	//test sport bike
 	motorbikeVehicle, err := motorbikeFactory.NewVehicle(SportMotorbikeType)
 	if err != nil {
 		t.Fatal(err)
@@ -21,8 +21,39 @@ func TestMotorbikeFactory(t *testing.T) {
 		t.Fatal("Struct assertion failed")
 	}
 
+	if sportBike.(Vehicle).NumWheels() != 2 {
+		t.Fatal("Wrong number of wheels generated for sport motorbike")
+	}
+
+	if sportBike.(Vehicle).NumSeats() != 2 {
+		t.Fatal("Wrong number of seats generated for sport motorbike")
+	}
 	//validate that it was a sport bike returend
 	if sportBike.GetMotorbikeType() != SportMotorbikeType {
+		t.Fatal("Wrong motorbike type generated")
+	}
+
+	//test cruise bike
+	motorbikeVehicle, err = motorbikeFactory.NewVehicle(CruiseMotorbikeType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cruiseBike, ok := motorbikeVehicle.(Motorbike)
+	if !ok {
+		t.Fatal("Struct assertion failed")
+	}
+
+	if cruiseBike.(Vehicle).NumWheels() != 2 {
+		t.Fatal("Wrong number of wheels generated for cruise motorbike")
+	}
+
+	if cruiseBike.(Vehicle).NumSeats() != 2 {
+		t.Fatal("Wrong number of seats generated for cruise motorbike")
+	}
+
+	//validate that it was a cruise bike returend
+	if cruiseBike.GetMotorbikeType() != CruiseMotorbikeType {
 		t.Fatal("Wrong motorbike type generated")
 	}
 }
